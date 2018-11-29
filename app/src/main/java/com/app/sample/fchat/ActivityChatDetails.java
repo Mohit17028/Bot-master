aea48272e4912cce8d0e11ab52facc3956e4cfcc
lakhina <ruchikalakhina191194@gmail.com>
2018-11-30 05:24:51 +0530

    attach images

--- app/src/main/java/com/app/sample/fchat/ActivityChatDetails.java
+++ app/src/main/java/com/app/sample/fchat/ActivityChatDetails.java
@@ -1,14 +1,19 @@
 package com.app.sample.fchat;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -31,6 +36,7 @@ import android.widget.LinearLayout;
 
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.sample.fchat.adapter.ChatDetailsListAdapter;
 import com.app.sample.fchat.data.ParseFirebaseData;
@@ -38,11 +44,17 @@ import com.app.sample.fchat.data.SettingsAPI;
 import com.app.sample.fchat.data.Tools;
 import com.app.sample.fchat.model.ChatMessage;
 import com.app.sample.fchat.model.Friend;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -61,6 +73,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -71,6 +84,12 @@ public class ActivityChatDetails extends AppCompatActivity implements RecordDial
     public static ChatDetailsListAdapter mAdapter;
     static String uid = "";
     private final int REQ_CODE_SPEECH_INPUT = 100;
+    private ImageView imageView;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private Uri filePath;
+
+    private final int PICK_IMAGE_REQUEST = 71;
 
     ParseFirebaseData pfbd;
     SettingsAPI set;
@@ -301,6 +320,10 @@ private View parent_view;
             public void onClick(View view){
                 attachment_layout.setVisibility(View.GONE);
                 // add code here.
+                storage = FirebaseStorage.getInstance();
+                storageReference = storage.getReference();
+                chooseImage();
+                uploadImage();
             }
         });
         add_video.setOnClickListener(new OnClickListener(){
@@ -569,7 +592,46 @@ private View parent_view;
         onBackPressed();
         return true;
     }*/
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    private void uploadImage() {
 
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
     private void hideKeyboard() {
         View view = this.getCurrentFocus();
         if (view != null) {
@@ -612,6 +674,19 @@ private View parent_view;
                     et_content.setText(ques);
 
                     //Toast.makeText(this,ques,Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case PICK_IMAGE_REQUEST: {
+                if (resultCode == RESULT_OK
+                        && data != null && data.getData() != null) {
+                    filePath = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                        imageView.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             }
